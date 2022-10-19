e1d9dca74c584b7a9253299a9101348bb3d9ce7c
Christina Sugiono <christina.sugiono95@gmail.com>
2022-10-19 11:52:59 +0800

    3

---
+++ 03-no-cdn-setup/index.js
@@ -0,0 +1,18 @@
+const navbar = (
+  <nav>
+    <div>
+      <h1>Welcome to React Course</h1>
+      <ul>
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  </nav>
+)
+
+
+ReactDOM.render(
+navbar,
+document.getElementById('root')
+)
